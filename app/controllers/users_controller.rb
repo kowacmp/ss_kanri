8b399539aa4170605f283a50a5c73d6ef5735830
KOWA <kowa@kowa-VirtualBox.(none)>
2012-08-06 10:28:04 +0900

    堤 カレンダー追加

--- app/controllers/users_controller.rb
+++
@@ -1,9 +0,0 @@
-class UsersController < ApplicationController
-  def sing_in
-    p "sing_in   sing_in   sing_in   sing_in   sing_in   sing_in"
-    aaa
-  end
-  def new_user_session_path
-    p "new_user_session_path   new_user_session_path   new_user_session_path"
-  end
-end
