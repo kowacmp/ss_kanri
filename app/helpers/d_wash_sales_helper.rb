96c0013c707b82b3f1486086d4b25d70ab3aa9a6
KeisukeHosooka <hosooka.keisuke@gmail.com>
2012-08-15 13:13:12 +0900

    洗車売上内訳データ 計算売上高表示 細岡

--- app/helpers/d_wash_sales_helper.rb
+++ app/helpers/d_wash_sales_helper.rb
@@ -1,3 +1,4 @@
+#coding: utf-8
 module DWashSalesHelper
   def get_m_washes
    MWash.find(:all, :conditions => ["deleted_flg is null or deleted_flg <> ?",1], :order => 'wash_cd')    
@@ -17,4 +18,17 @@ module DWashSalesHelper
     DWashsaleItem.find(:all, 
     :conditions => ["d_wash_sale_id= ? and m_wash_id = ? and wash_no = ?",d_wash_sale_id,m_wash_id,wash_no]).first
   end
+  
+  def keisan_uriage(d_washsales_item,d_washsales_item_yesterday)
+    unless d_washsales_item == nil || d_washsales_item_yesterday == nil
+      if d_washsales_item.meter != 0 && d_washsales_item_yesterday.meter != 0
+        d_washsales_item.meter - d_washsales_item_yesterday.meter
+          else
+          0
+          end
+        else
+          0
+        end
+  end
+  
 end
