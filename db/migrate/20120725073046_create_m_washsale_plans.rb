09f69ce0a18d3bfc20f78588309704774a67ab01
KeisukeHosooka <hosooka.keisuke@gmail.com>
2012-08-20 09:36:55 +0900

    初期値追加 細岡

--- db/migrate/20120725073046_create_m_washsale_plans.rb
+++ db/migrate/20120725073046_create_m_washsale_plans.rb
@@ -1,18 +1,20 @@
+# -*- coding:utf-8 -*-
+#初期値追加　細岡
 class CreateMWashsalePlans < ActiveRecord::Migration
   def change
     create_table :m_washsale_plans do |t|
-      t.integer :m_shop_id
-      t.integer :m_wash_id
-      t.integer :sunday
-      t.integer :monday
-      t.integer :tuesday
-      t.integer :wednesday
-      t.integer :thursday
-      t.integer :friday
-      t.integer :saturday
-      t.integer :created_user_id
-      t.integer :updated_user_id
-      t.integer :deleted_flg
+      t.integer :m_shop_id,:null => false
+      t.integer :m_wash_id,:null => false
+      t.integer :sunday   ,:default => 0,:limit => 1 
+      t.integer :monday   ,:default => 0,:limit => 1
+      t.integer :tuesday  ,:default => 0,:limit => 1
+      t.integer :wednesday,:default => 0,:limit => 1
+      t.integer :thursday ,:default => 0,:limit => 1
+      t.integer :friday   ,:default => 0,:limit => 1
+      t.integer :saturday ,:default => 0,:limit => 1
+      t.integer :created_user_id,:default => 0
+      t.integer :updated_user_id,:default => 0
+      t.integer :deleted_flg ,:default => 0
       t.timestamp :deleted_at
 
       t.timestamps
