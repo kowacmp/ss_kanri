b43047e91d4a892aac478e568420a8da57f65128
KOWA <kowa@kowa-VirtualBox.(none)>
2012-08-22 13:24:42 +0900

    2012/08/22 oda

--- db/migrate/20120725062238_create_d_comments.rb
+++ db/migrate/20120725062238_create_d_comments.rb
@@ -6,6 +6,7 @@ class CreateDComments < ActiveRecord::Migration
       t.integer   :menu_id,         :null => false
       t.string    :contents,        :null => false, :limit => 40
       t.integer   :receive_id,      :null => false
+      t.integer   :send_id,         :null => false
       t.integer   :created_user_id, :null => false
 
       t.timestamps
@@ -15,6 +16,7 @@ class CreateDComments < ActiveRecord::Migration
              COMMENT ON COLUMN d_comments.menu_id IS 'メニューid';
              COMMENT ON COLUMN d_comments.contents IS '内容';
              COMMENT ON COLUMN d_comments.receive_id IS '受信ユーザーid';
+             COMMENT ON COLUMN d_comments.send_id IS '送信ユーザーid';
              COMMENT ON COLUMN d_comments.created_user_id IS '作成者id ： user_id';
     "    
   end 
