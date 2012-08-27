030e3d1412d873af8dea90d6dc5cc3059d10086e
KeisukeHosooka <hosooka.keisuke@gmail.com>
2012-08-27 17:43:36 +0900

    実績集計レポート画面作成 細岡

--- app/controllers/d_tank_decrease_reports_controller.rb
+++ app/controllers/d_tank_decrease_reports_controller.rb
@@ -1,5 +1,10 @@
 class DTankDecreaseReportsController < ApplicationController
   def index
+          search 
+  end
+
+  def search
+    @rbtn = params[:rbtn]
     if params[:input_ymd] == nil
       @input_ymd = Time.now.strftime("%Y/%m/%d")
     else
@@ -7,17 +12,9 @@ class DTankDecreaseReportsController < ApplicationController
     end
     @input_ymd_s = @input_ymd.delete("/")
     
-    @reports = DTankDecreaseReport.find_by_sql(['select a.*,b.result_date,b.kakutei_flg,c.id as shop_id,
-           c.shop_cd,c.shop_name,c.shop_ryaku,d.group_name
-           from d_tank_decrease_reports a,d_results b,m_shops c,m_shop_groups d
-           where a.d_result_id = b.id
-             and b.m_shop_id = c.id
-             and c.m_shop_group_id = d.id
-             and b.result_date = ?
-           order by m_shop_group_id,c.id',@input_ymd_s])
-           
+    @groups = MShopGroup.find(:all,[:conditions => 'deleted_flg = 0',:order => 'group_cd'])
+    
   end
-
   def print
   end
 
--- app/helpers/d_tank_decrease_reports_helper.rb
+++ app/helpers/d_tank_decrease_reports_helper.rb
@@ -1,2 +1,13 @@
 module DTankDecreaseReportsHelper
+  def get_title(id)
+    MOil.find(:all,:conditions => ['deleted_flg = 0 and id = ?',id]).first.oil_name
+  end
+  
+  def get_shops(group_id)
+    MShop.find(:all,:conditions => ['m_shop_group_id = ?',group_id],:order => 'shop_cd')
+  end
+  
+  def get_d_result(shop_id,result_date)
+    DResult.find(:all,:conditions => ['m_shop_id = ? and result_date = ?',shop_id,result_date]).first
+  end
 end
