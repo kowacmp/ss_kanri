d2ac120070ff276451fc7ac558262f11e3c93542
KeisukeHosooka <hosooka.keisuke@gmail.com>
2012-08-27 13:40:19 +0900

    pullのため、コミット 細岡

--- app/controllers/d_tank_decrease_reports_controller.rb
+++ app/controllers/d_tank_decrease_reports_controller.rb
@@ -7,14 +7,15 @@ class DTankDecreaseReportsController < ApplicationController
     end
     @input_ymd_s = @input_ymd.delete("/")
     
-    @rerports = DTankDecreaseReport.find_by_sql(['select a.*,b.result_date,b.kakutei_flg,c.id as shop_id,
+    @reports = DTankDecreaseReport.find_by_sql(['select a.*,b.result_date,b.kakutei_flg,c.id as shop_id,
            c.shop_cd,c.shop_name,c.shop_ryaku,d.group_name
            from d_tank_decrease_reports a,d_results b,m_shops c,m_shop_groups d
            where a.d_result_id = b.id
              and b.m_shop_id = c.id
-             and a.m_shop_group_id = d.id
+             and c.m_shop_group_id = d.id
              and b.result_date = ?
            order by m_shop_group_id,c.id',@input_ymd_s])
+           
   end
 
   def print
