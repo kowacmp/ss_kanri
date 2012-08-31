3f13139c395355de3be9e8973e001f0120d7a7f8
KOWA <kowa@kowa-VirtualBox.(none)>
2012-08-31 21:43:58 +0900

    2012/08/31 oda

--- app/controllers/d_fixture_approvals_controller.rb
+++ app/controllers/d_fixture_approvals_controller.rb
@@ -2,23 +2,24 @@ class DFixtureApprovalsController < ApplicationController
   def index
     menu = Menu.find(:all,:conditions => ['uri = ?','d_fixture_approvals']).first
     m_approval = MApproval.find(:all,:conditions => ['menu_id = ?',menu.id]).first
-    if m_approval.approval_id1 == current_user.id
-      @appraval_id = m_approval.approval_id1
-      @approval_name = m_approval.approval_name1
-    elsif m_approval.approval_id2 == current_user.id
-      @appraval_id = m_approval.approval_id2
-      @approval_name = m_approval.approval_name2
-    elsif m_approval.approval_id3 == current_user.id
-      @appraval_id = m_approval.approval_id3
-      @approval_name = m_approval.approval_name3
-    elsif m_approval.approval_id4 == current_user.id
-      @appraval_id = m_approval.approval_id4
-      @approval_name = m_approval.approval_name4
-    elsif m_approval.approval_id5 == current_user.id
-      @appraval_id = m_approval.approval_id5
-      @approval_name = m_approval.approval_name5
-    end
-    
+    unless m_approval == nil    
+      if m_approval.approval_id1 == current_user.id
+        @appraval_id = m_approval.approval_id1
+        @approval_name = m_approval.approval_name1
+      elsif m_approval.approval_id2 == current_user.id
+        @appraval_id = m_approval.approval_id2
+        @approval_name = m_approval.approval_name2
+      elsif m_approval.approval_id3 == current_user.id
+        @appraval_id = m_approval.approval_id3
+        @approval_name = m_approval.approval_name3
+      elsif m_approval.approval_id4 == current_user.id
+        @appraval_id = m_approval.approval_id4
+        @approval_name = m_approval.approval_name4
+      elsif m_approval.approval_id5 == current_user.id
+        @appraval_id = m_approval.approval_id5
+        @approval_name = m_approval.approval_name5    
+      end
+    end    
     search
     #unless @appraval_id == nil
     #  @fixtures = DFixture.find(:all,:order => 'application_date,id')
