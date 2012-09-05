91842b95df8103b03628dc9f890407e1285d8b55
KOWA <kowa@kowa-VirtualBox.(none)>
2012-09-05 21:35:35 +0900

    2012/09/05 oda

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,8 @@ SsKanri::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
-
+  config.assets.precompile += %w(*.js)
+  config.assets.precompile += %w(*.css) 
   # Generate digests for assets URLs
   config.assets.digest = true
 
