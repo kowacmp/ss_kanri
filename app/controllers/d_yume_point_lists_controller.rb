458ea8c84ebe90e397ff41c1b36234edc7412693
KeisukeHosooka <hosooka.keisuke@gmail.com>
2012-09-06 09:32:33 +0900

    夢ポイントリスト 新規作成 細岡

---
+++ app/controllers/d_yume_point_lists_controller.rb
@@ -0,0 +1,11 @@
+class DYumePointListsController < ApplicationController
+  def index
+  end
+
+  def search
+  end
+
+  def print
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 SsKanri::Application.routes.draw do
 
+  get "d_yume_point_lists/index"
+
+  get "d_yume_point_lists/search"
+
+  get "d_yume_point_lists/print"
+
   get "d_tank_compute_report_details/index"
   get "d_tank_compute_report_details/search"
   get "d_tank_compute_report_details/print"
