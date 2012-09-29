d038c5c619ed6e9e923e5ed7abbe848a000315d6
KOWA <kowa@kowa-VirtualBox.(none)>
2012-09-29 17:41:29 +0900

    2012/09/29 日付の規定値を前日へ、対象日へラベル変更

--- app/controllers/d_result_reports_controller.rb
+++ app/controllers/d_result_reports_controller.rb
@@ -10,7 +10,9 @@ class DResultReportsController < ApplicationController
   def search
     
     if params[:input_ymd] == nil
-      @input_ymd = Time.now.strftime("%Y/%m/%d")
+      # UPDATE 2012.09.29 日付の規定値は前日
+      #@input_ymd = Time.now.strftime("%Y/%m/%d")
+      @input_ymd = (Time.now - 60*60*24).strftime("%Y/%m/%d")
     else
       @input_ymd = params[:input_ymd]
     end
