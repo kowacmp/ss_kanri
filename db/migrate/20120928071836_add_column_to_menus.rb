059122ec6428f38472924d5f48f0feb531e6fa72
KOWA <kowa@kowa-VirtualBox.(none)>
2012-10-03 22:24:27 +0900

    fuchigami

--- db/migrate/20120928071836_add_column_to_menus.rb
+++ db/migrate/20120928071836_add_column_to_menus.rb
@@ -13,11 +13,12 @@ class AddColumnToMenus < ActiveRecord::Migration
     execute "update menus set permission_actions = 'index?audit_class=1,edit,dialog_gosa' where uri='d_audit_etcs?audit_class=1';"
     execute "update menus set permission_actions = 'index?audit_class=0,edit,dialog_gosa' where uri='d_audit_washes?audit_class=0';"
     execute "update menus set permission_actions = 'index?audit_class=1,edit,dialog_gosa' where uri='d_audit_washes?audit_class=1';"
-    execute "update menus set permission_actions = 'new,meter_index,tank_index,yume_index,yume_new,yume_edit,yume_delete,select_date' where uri='d_results/new';"
+    execute "update menus set permission_actions = 'new,meter_index,tank_index,yume_index,yume_new,yume_edit,yume_delete,select_date,oil_total_set' where uri='d_results/new';"
     execute "update menus set permission_actions = 'new,report_view' where uri='d_sales/new';"
     execute "update menus set permission_actions = 'index?mode=edit,edit' where uri='d_washpurika_reports?mode=edit';"
     execute "update menus set permission_actions = 'index?mode=show' where uri='d_washpurika_reports?mode=show';"
-    
+    execute "update menus set permission_actions = 'index,show,compare,d_wash_sales/index,d_wash_sales/entry_error' where uri='d_washsale_lists';"
+    execute "update menus set permission_actions = 'index,show,new,edit,m_meters/new' where uri='m_shops';"
   end
 
   def down
