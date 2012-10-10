77f8d3016d92621ba10d31d6120736c0307abeb5
KOWA <kowa@kowa-VirtualBox.(none)>
2012-10-10 17:09:22 +0900

    2012/10/10 監査リスト追加

--- app/controllers/d_audit_lists_controller.rb
+++ app/controllers/d_audit_lists_controller.rb
@@ -1,3 +1,4 @@
+# -*- coding:utf-8 -*-
 class DAuditListsController < ApplicationController
 
   def index
@@ -15,6 +16,7 @@ class DAuditListsController < ApplicationController
     case params[:header][:kansa].to_s
     when "1" #金庫
       @table = "d_audit_cashboxes"
+      @shori_name = "金庫"
 
       if params[:header][:audit_class].to_s == "0" then
         @menu_id = "38"
@@ -24,6 +26,7 @@ class DAuditListsController < ApplicationController
       
     when "2" #釣銭
       @table = "d_audit_changemachines"
+      @shori_name = "釣銭"
 
       if params[:header][:audit_class].to_s == "0" then
         @menu_id = "39"
@@ -33,6 +36,7 @@ class DAuditListsController < ApplicationController
     
     when "3" #洗車
       @table = "d_audit_washes"
+      @shori_name = "洗車"
 
       if params[:header][:audit_class].to_s == "0" then
         @menu_id = "40"
@@ -42,6 +46,7 @@ class DAuditListsController < ApplicationController
     
     when "4" #その他
       @table = "d_audit_etcs"
+      @shori_name = "その他"
 
       if params[:header][:audit_class].to_s == "0" then
         @menu_id = "42"
@@ -51,10 +56,92 @@ class DAuditListsController < ApplicationController
     
     end
     
+    # audit_date_from～to と audit_date の切り分け
+    audit_date = @p_header[:audit_date].to_s.delete("/")
+    case @table
+    when "d_audit_etcs", "d_audit_washes"
+      w_audit_date = " audit_date_from <= '#{ audit_date }' and audit_date_to >= '#{ audit_date }' "
+    else
+      w_audit_date = " audit_date = '#{ audit_date }' "
+    end
     
+    # SQL文組み立て
+    @sql = "
+      select
+         * 
+      from 
+        #{ @table }
+      where 
+            #{ w_audit_date }
+        and audit_class = #{ @p_header[:audit_class] }
+    "
     
+    # 店舗読込
+    if @p_header[:shop_kbn].to_s == "" then
+      @m_shops = MShop.find(:all, :conditions => ["shop_kbn<>9"], :order => "shop_cd")
+    else
+      @m_shops = MShop.find(:all, :conditions => ["shop_kbn=?", @p_header[:shop_kbn]], :order => "shop_cd")
+    end
+      
+  end
+
+  # AJAX LOCK処理(単体)
+  def lock
+    
+    audit = get_audit_record(params[:table], params[:id])
+    
+    if params[:kakutei_flg].to_s == "checked" then
+      audit.kakutei_flg = 1
+    else
+      audit.kakutei_flg = 0
+    end
+    
+    audit.updated_user_id = current_user.id
+    audit.save!
+    
+    head :ok
+    
+  end
+  
+  # AJAX LOCK処理(複数件)
+  def lock_all
+    
+    ids = params[:ids].to_s.split(",")
+    for id in ids
+      
+      audit = get_audit_record(params[:table], id)
+      
+      if params[:kakutei_flg].to_s == "checked" then
+        audit.kakutei_flg = 1
+      else
+        audit.kakutei_flg = 0
+      end
+      
+      audit.updated_user_id = current_user.id
+      audit.save!
+    end
+    
+    head :ok
+    
+  end
+  
+private
+  # AJAX LOCK 対象のActiveRecord取得
+  def get_audit_record(table, id)
+    
+    case table
+    when "d_audit_cashboxes"
+      audit = DAuditCashbox.find(id)
+    when "d_audit_changemachines"
+      audit = DAuditChangemachine.find(id)
+    when "d_audit_washes"
+      audit = DAuditWash.find(id)
+    when "d_audit_etcs"
+      audit = DAuditEtc.find(id)
+    end
+    
+    return audit
     
   end
 
- 
 end
--- app/helpers/d_audit_lists_helper.rb
+++ app/helpers/d_audit_lists_helper.rb
@@ -1,2 +1,71 @@
+# -*- coding:utf-8 -*-
 module DAuditListsHelper
+
+  # 監査日取得
+  def get_audit_date(rec)
+  
+    if rec.nil? then
+      return ""
+    else
+      if rec[:audit_date_from].nil? then
+        audit_date = rec[:audit_date].to_s
+        return audit_date[0..3] + "/" + audit_date[4..5] + "/" + audit_date[6..7]
+      else
+        audit_date_from = rec[:audit_date_from].to_s
+        audit_date_to = rec[:audit_date_to].to_s
+      
+        ret = audit_date_from[0..3] + "/" + audit_date_from[4..5] + "/" + audit_date_from[6..7] + "<br/>" +
+          "～" + audit_date_to[0..3] + "/" + audit_date_to[4..5] + "/" + audit_date_to[6..7]
+        
+        return raw ret
+      
+      end
+    end
+  end
+  
+  # 種別取得
+  def get_audit_class(rec)
+  
+    if rec.nil? then
+      return ""
+    else
+      return MCode.find(:first, :conditions => ["kbn='audit_class' and code = ? ", rec[:audit_class].to_s])[:code_name]
+    end
+  
+  end
+  
+  # 監査人取得
+  def get_kansanin(rec)
+  
+    if rec.nil? then
+      return ""
+    else
+      return User.find(rec[:created_user_id])[:user_name].to_s
+    end
+  
+  end
+  
+  # 立会人取得
+  def get_tatiainin(rec)
+  
+    if rec.nil? then
+      return ""
+    else
+      return User.find(rec[:confirm_user_id])[:user_name].to_s
+    end
+  
+  end
+  
+  # 入力状況
+  def get_nyuryoku(rec)
+    
+    if rec.nil? then
+      return "未"
+    else
+      return "済"
+    end
+  
+  
+  end 
+  
 end
