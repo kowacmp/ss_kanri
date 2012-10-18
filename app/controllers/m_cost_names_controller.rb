4cddefeb44bb1cb0c4363eb3b794e721293814c6
KOWA <kowa@kowa-VirtualBox.(none)>
2012-10-18 14:50:44 +0900

    西村　人件費項目名称マスタ　追加

--- app/controllers/m_cost_names_controller.rb
+++ app/controllers/m_cost_names_controller.rb
@@ -2,7 +2,29 @@ class MCostNamesController < ApplicationController
   # GET /m_cost_names
   # GET /m_cost_names.json
   def index
-    @m_cost_names = MCostName.all
+    #@m_cost_names = MCostName.all
+    
+    select_sql = "select a.*, b.shop_cd,b.shop_name "
+    select_sql << " ,(case a.monthly_name1 when '' then 0 else 1 end + case a.monthly_name2 when '' then 0 else 1 end + "
+    select_sql << "   case a.monthly_name3 when '' then 0 else 1 end + case a.monthly_name4 when '' then 0 else 1 end + "
+    select_sql << "   case a.monthly_name5 when '' then 0 else 1 end + case a.monthly_name6 when '' then 0 else 1 end "
+    select_sql << "   ) as monthly_name_count "
+    select_sql << " ,(case a.hour_name1 when '' then 0 else 1 end + case a.hour_name2 when '' then 0 else 1 end + "
+    select_sql << "   case a.hour_name3 when '' then 0 else 1 end + case a.hour_name4 when '' then 0 else 1 end + "
+    select_sql << "   case a.hour_name5 when '' then 0 else 1 end + case a.hour_name6 when '' then 0 else 1 end "
+    select_sql << "   ) as hour_name_count "
+    select_sql << " ,(case a.day_name1 when '' then 0 else 1 end + case a.day_name2 when '' then 0 else 1 end "
+    select_sql << "   ) as day_name_count "
+    select_sql << " ,(case a.input_name1 when '' then 0 else 1 end + case a.input_name2 when '' then 0 else 1 end + "
+    select_sql << "   case a.input_name3 when '' then 0 else 1 end + case a.input_name4 when '' then 0 else 1 end "
+    select_sql << "   ) as input_name_count "
+    select_sql << " from m_cost_names a " 
+    select_sql << " left join (select * from m_shops) b on a.m_shop_id = b.id "
+    
+    condition_sql = ""
+    
+    @m_cost_names = MCostName.find_by_sql("#{select_sql} #{condition_sql} order by b.shop_cd")
+    
 
     respond_to do |format|
       format.html # index.html.erb
@@ -44,7 +66,8 @@ class MCostNamesController < ApplicationController
 
     respond_to do |format|
       if @m_cost_name.save
-        format.html { redirect_to @m_cost_name, notice: 'M cost name was successfully created.' }
+        format.html { redirect_to :controller => "m_cost_names", :action => "index" }
+        #format.html { redirect_to @m_cost_name, notice: 'M cost name was successfully created.' }
         format.json { render json: @m_cost_name, status: :created, location: @m_cost_name }
       else
         format.html { render action: "new" }
@@ -60,7 +83,8 @@ class MCostNamesController < ApplicationController
 
     respond_to do |format|
       if @m_cost_name.update_attributes(params[:m_cost_name])
-        format.html { redirect_to @m_cost_name, notice: 'M cost name was successfully updated.' }
+        format.html { redirect_to :controller => "m_cost_names", :action => "index" }
+        #format.html { redirect_to @m_cost_name, notice: 'M cost name was successfully updated.' }
         format.json { head :ok }
       else
         format.html { render action: "edit" }
