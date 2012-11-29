88bf232ae59b40ada2ab919e96c3555726095ab7
KOWA <kowa@kowa-VirtualBox.(none)>
2012-11-29 17:53:02 +0900

    ikuno

--- db/migrate/20121129015329_add_column_d_price_checks2.rb
+++ db/migrate/20121129015329_add_column_d_price_checks2.rb
@@ -1,20 +1,20 @@
 # -*- coding:utf-8 -*-
 class AddColumnDPriceChecks2 < ActiveRecord::Migration
   def up
-    rename_column :d_price_checks, :minus_name4, :minus_name5
-    add_column    :d_price_checks, :minus_name4, :string, :limit => 10
+    rename_column :d_price_checks, :minus_name4, :minus_name6
     add_column    :d_price_checks, :minus_gak4,  :decimal, :precision => 3, :scale => 1, :default => 0
+    add_column    :d_price_checks, :minus_gak5,  :decimal, :precision => 3, :scale => 1, :default => 0
     
-    execute "COMMENT ON COLUMN d_price_checks.minus_name5 IS '予備';"
-    execute "COMMENT ON COLUMN d_price_checks.minus_name4 IS 'マイナス名称4　プリカ(灯油)';"
-    execute "COMMENT ON COLUMN d_price_checks.minus_gak4  IS 'マイナス金額4';"
+    execute "COMMENT ON COLUMN d_price_checks.minus_name6 IS '備考';"
+    execute "COMMENT ON COLUMN d_price_checks.minus_gak4  IS 'マイナス金額4　プリカ(灯油)';"
+    execute "COMMENT ON COLUMN d_price_checks.minus_gak5  IS 'マイナス金額5　会員(灯油)';"
     
   end
 
   def down
-    remove_column :d_price_checks, :minus_name4
     remove_column :d_price_checks, :minus_gak4
-    rename_column :d_price_checks, :minus_name5, :minus_name4 
+    remove_column :d_price_checks, :minus_gak5
+    rename_column :d_price_checks, :minus_name6, :minus_name4 
     
     execute "COMMENT ON COLUMN d_price_checks.minus_name4 IS '予備';"
   end
