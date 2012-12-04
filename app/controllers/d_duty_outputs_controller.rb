0195624a8f2b4b5d0e983a5dc37260b68b66edd3
KOWA <kowa@kowa-VirtualBox.(none)>
2012-12-04 16:29:00 +0900

    給与データ出力対応 小田

--- app/controllers/d_duty_outputs_controller.rb
+++ app/controllers/d_duty_outputs_controller.rb
@@ -96,4 +96,177 @@ class DDutyOutputsController < ApplicationController
    
   end
 
+  def print
+    
+    show()
+    
+    report = ThinReports::Report.new :layout =>  File.join(Rails.root,'app','reports', 'd_salary_list.tlf')
+
+    #ページ番号、タイトル、作成日セット  
+    title1 = "#{@year}年#{@month}月"
+    title2 = "#{MShop.find(@m_shop_id).shop_ryaku}"
+
+    report.events.on :page_create do |e|
+      e.page.item(:page).value(e.page.no)
+      e.page.item(:sakusei_ymd).value(Time.now.strftime("%Y-%m-%d"))
+      e.page.item(:title1).value(title1)
+      e.page.item(:title2).value(title2)
+      start_day.times do |i|
+        e.page.item("d_#{i+1}").value(i+1)
+      end      
+    end #evants.on
+    
+    report.start_new_page
+     report.start_new_page do |page|
+        page.item(:hour_name1).value(@m_cost_name.hour_name1)
+        page.item(:hour_name2).value(@m_cost_name.hour_name2)
+        page.item(:hour_name3).value(@m_cost_name.hour_name3)
+        page.item(:hour_name4).value(@m_cost_name.hour_name4)
+        page.item(:hour_name5).value(@m_cost_name.hour_name5)
+        page.item(:hour_name6).value(@m_cost_name.hour_name6)
+        page.item(:day_name1).value(@m_cost_name.day_name1)
+        page.item(:day_name2).value(@m_cost_name.day_name2)
+        page.item(:name1).value(@m_cost_name.input_name1)
+        page.item(:name2).value(@m_cost_name.input_name2)
+        page.item(:name3).value(@m_cost_name.input_name3)
+        page.item(:name4).value(@m_cost_name.input_name4)
+    end
+    # 詳細作成
+    @d_duties.each do |d_duty|
+    # Set header datas.
+      report.page.list(:list).add_row do |row|
+          #社員ｺｰﾄﾞ
+          row.item(:u_code).value(shop.shop_ryaku) 
+          #氏名
+          row.item(:u_name).value(d_duty.user_name) 
+          #出勤日数
+          row.item(:d_cnt).value(d_duty.day) 
+          #時間1
+          row.item(:d_time1).value(d_duty.day_work_time) 
+          #単価1
+          row.item(:g_price1).value(d_duty.general_price) 
+          #金額1
+          row.item(:g_money1).value(get_general_money(d_duty)) 
+          #時間2
+          row.item(:d_time2).value(d_duty.day_over_time) 
+          #単価2
+          row.item(:g_price2).value(d_duty.general_overtime) 
+          #金額2
+          row.item(:g_money2).value(get_general_overtime_money(d_duty))
+          #時間3
+          row.item(:d_time3).value(d_duty.night_work_time) 
+          #単価3
+          row.item(:g_price3).value(d_duty.night_price) 
+          #金額3
+          row.item(:g_money3).value(get_night_money(d_duty)) 
+          #時間4
+          row.item(:d_time4).value(d_duty.night_over_time) 
+          #単価4
+          row.item(:g_price4).value(d_duty.night_overtime) 
+          #金額4
+          row.item(:g_money4).value(get_night_overtime_money(d_duty)) 
+          #時間手当1
+          row.item(:t_price1).value(d_duty.time_price1) 
+          #時間手当1金額
+          row.item(:t_money1).value(d_duty.time1_money) 
+          #時間手当2
+          row.item(:t_price2).value(d_duty.time_price2) 
+          #時間手当2金額
+          row.item(:t_money2).value(d_duty.time2_money) 
+          #時間手当3
+          row.item(:t_price3).value(d_duty.time_price3) 
+          #時間手当3金額
+          row.item(:t_money3).value(d_duty.time3_money) 
+          #時間手当4
+          row.item(:t_price4).value(d_duty.time_price4) 
+          #時間手当4金額
+          row.item(:t_money4).value(d_duty.time4_money) 
+          #時間手当5
+          row.item(:t_price5).value(d_duty.time_price5) 
+          #時間手当5金額
+          row.item(:t_money5).value(d_duty.time5_money) 
+          #時間手当6
+          row.item(:t_price6).value(d_duty.time_price6) 
+          #時間手当6金額
+          row.item(:t_money6).value(d_duty.time6_money) 
+          #日数手当1
+          row.item(:d_price1).value(d_duty.day_price1) 
+          #日数手当1金額
+          row.item(:d_money1).value(d_duty.day1_money) 
+          #日数手当2
+          row.item(:d_price2).value(d_duty.day_price2) 
+          #時間手当2金額
+          row.item(:d_money2).value(d_duty.day2_money) 
+          #月間手当金額1
+          row.item(:m_money1).value(d_duty.get_money1) 
+          #月間手当金額2
+          row.item(:m_money2).value(d_duty.get_money2) 
+          #月間手当金額3
+          row.item(:m_money3).value(d_duty.get_money3) 
+          #月間手当金額4
+          row.item(:m_money4).value(d_duty.get_money4) 
+          #支払金額
+          row.item(:sum_money).value(get_shiharai_gak(d_duty)) 
+      end #add_row
+    end # shops.each
+    
+    report.layout.config.list(:list) do
+    # フッターに合計をセット.
+      events.on :footer_insert do |e|
+        #時間1
+        e.section.item(:d_time1_sum).value(get_day_work_times(@d_duties))
+        #金額1
+        e.section.item(:g_money1_sum).value(get_general_moneys(@d_duties))
+        #時間2
+        e.section.item(:d_time2_sum).value(get_day_over_times(@d_duties))
+        #金額2
+        e.section.item(:g_money2_sum).value(get_general_overtime_moneys(@d_duties))
+        #時間3
+        e.section.item(:d_time3_sum).value(get_night_work_times(@d_duties))
+        #金額3
+        e.section.item(:g_money3_sum).value(get_night_moneys(@d_duties)) 
+        #時間4
+        e.section.item(:t_money4_sum).value(gget_night_overtimes(@d_duties))
+        #金額4
+        e.section.item(:g_money4_sum).value(get_night_overtime_moneys(@d_duties)) 
+        #時間手当1金額
+        e.section.item(:t_money1_sum).value(get_time1_moneys(@d_duties)) 
+        #時間手当2金額
+        e.section.item(:t_money2_sum).value(get_time2_moneys(@d_duties)) 
+        #時間手当3金額
+        e.section.item(:t_money3_sum).value(get_time3_moneys(@d_duties)) 
+        #時間手当4金額
+        e.section.item(:t_money4_sum).value(get_time4_moneys(@d_duties)) 
+        #時間手当5金額
+        e.section.item(:t_money5_sum).value(get_time5_moneys(@d_duties)) 
+        #時間手当6金額
+        e.section.item(:t_money6_sum).value(get_time6_moneys(@d_duties)) 
+        #日数手当1金額
+        e.section.item(:d_money1_sum).value(get_day1_money(@d_duties)) 
+        #日数手当2金額
+        e.section.item(:d_money2_sum).value(get_day2_money(@d_duties)) 
+        #月間手当金額1
+        e.section.item(:m_money1_sum).value(get_moneys1(@d_duties)) 
+        #月間手当金額2
+        e.section.item(:m_money2_sum).value(get_moneys2(@d_duties)) 
+        #月間手当金額3
+        e.section.item(:m_money3_sum).value(get_moneys3(@d_duties)) 
+        #月間手当金額4
+        e.section.item(:m_money4_sum).value(get_moneys4(@d_duties)) 
+        #支払金額
+        e.section.item(:all_money).value(get_shiharai_gaks(@d_duties)) 
+     end #events.on
+    end #list
+    #ファイル名セット     
+    pdf_title = "給与内訳リスト_#{@year}年#{@month}月_title2.pdf"
+    ua = request.env["HTTP_USER_AGENT"]
+    pdf_title = URI.encode(pdf_title) if ua.include?('MSIE') #InternetExproler対応
+      
+    #pdfを作成
+    send_data report.generate, :filename    => pdf_title ,#URI.encode(pdf_title), #KOWA IEでファイルをダウンロードすると文字化けするため
+                               :type        => 'application/pdf',
+                               :disposition => 'attachment'
+                               
+  end #def print
+
 end
