b5b8f8d8adf7c147393230aa37d62aad8825a6d0
KOWA <kowa@kowa-VirtualBox.(none)>
2013-04-09 10:18:53 +0900

    2013.04.09 ikuno 月間洗車機売上報告書一覧 確認チェック更新時の処理を厳密に行うように修正

--- app/controllers/d_washsale_report_lists_controller.rb
+++ app/controllers/d_washsale_report_lists_controller.rb
@@ -46,22 +46,34 @@ class DWashsaleReportListsController < ApplicationController
   def update
     @sale_date   = params[:sale_date]
     
-    @d_washsale_reports = DWashsaleReport.find(:all,:conditions => ['sale_date = ?',@sale_date])
+    # UPDATE BEGIN 2013.04.09 更新対象を厳密にする
     
-      @d_washsale_reports.each do |d_washsale_report|
-        unless params[:cbox] == nil
-          if params[:cbox].include?(d_washsale_report.id.to_s)
-            d_washsale_report.kakutei_flg = 1
-          else
-            d_washsale_report.kakutei_flg = 0
-          end #if
-        else
-          d_washsale_report.kakutei_flg = 0
-        end # uless
-      d_washsale_report.updated_user_id = current_user.id
+    #@d_washsale_reports = DWashsaleReport.find(:all,:conditions => ['sale_date = ?',@sale_date])
+    # 
+    #@d_washsale_reports.each do |d_washsale_report|
+    #    unless params[:cbox] == nil
+    #      if params[:cbox].include?(d_washsale_report.id.to_s)
+    #        d_washsale_report.kakutei_flg = 1
+    #      else
+    #        d_washsale_report.kakutei_flg = 0
+    #      end #if
+    #    else
+    #      d_washsale_report.kakutei_flg = 0
+    #    end # uless
+    #  d_washsale_report.updated_user_id = current_user.id
+    #  d_washsale_report.save!
+    #end
+    
+    params[:kakutei_flgs].each do |id, kakutei_flg|
+      d_washsale_report = DWashsaleReport.find(id)
+      d_washsale_report[:kakutei_flg] = kakutei_flg
+      d_washsale_report[:updated_user_id] = current_user.id
       d_washsale_report.save!
     end
-                
+    @d_washsale_reports = DWashsaleReport.find(:all,:conditions => ['sale_date = ?',@sale_date])
+    
+    # UPDATE END 2013.04.09 更新対象を厳密にする
+    
     respond_to do |format|
      @time_now = (@sale_date[0,4] + "/" + @sale_date[4,2] + "/01").to_time
      @shop_kbn = params[:shop_kbn]
