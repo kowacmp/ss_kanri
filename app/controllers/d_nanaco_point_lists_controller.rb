f79ba29effa2cf1fa55d4e840ddb580f8104182c
KOWA <kowa@kowa-VirtualBox.(none)>
2013-12-05 18:33:33 +0900

    nanacoポイント性能強化修正

--- app/controllers/d_nanaco_point_lists_controller.rb
+++ app/controllers/d_nanaco_point_lists_controller.rb
@@ -10,12 +10,12 @@ class DNanacoPointListsController < ApplicationController
     @time_now = Time.now
     
     if params[:start_ymd] == nil or params[:start_ymd] == ""
-      @start_ymd = (Time.now - 60*60*24).strftime("%Y/%m/%d")
+      @start_ymd   = (Time.now).prev_month.strftime("%Y/%m/%d")
     else
       @start_ymd = params[:start_ymd]
     end
     if params[:end_ymd] == nil or params[:end_ymd] == ""
-      @end_ymd   = (Time.now - 60*60*24*2).next_month.strftime("%Y/%m/%d")
+      @end_ymd = (Time.now - 60*60*24).strftime("%Y/%m/%d")
     else
       @end_ymd   = params[:end_ymd]     
     end
@@ -58,6 +58,20 @@ class DNanacoPointListsController < ApplicationController
     sum_sp1 = 0
     sum_sp2 = 0
 
+
+    nanaco_datas = Array::new
+    @num = Date.parse(@to_ymd) - Date.parse(@from_ymd)
+    for i in 0..@num.to_i
+      result_date = (Date.parse(@from_ymd) + i).strftime("%Y%m%d")
+      nanaco_point_data = get_nanaco_point(result_date,@shop_kbn)
+      nanaco_point_data.each_with_index do |nanaco,idx|
+       if i == 0
+         nanaco_datas[idx] = Hash::new
+       end
+       nanaco_datas[idx][i] = nanaco
+      end
+    end
+    
     shops = MShop.find(:all,:conditions => ['shop_kbn = ?', @shop_kbn],:order => 'shop_cd')
     
     report = ThinReports::Report.new :layout =>  File.join(Rails.root,'app','reports', 'd_nanaco_point_list.tlf')
@@ -91,30 +105,27 @@ class DNanacoPointListsController < ApplicationController
     report.start_new_page
  
     # 詳細作成
-    shops.each do |shop|
+    nanaco_datas.each_with_index do |nanaco,idx|
       result_date = @from_ymd
       p1_sum_col = 0
       p2_sum_col = 0
       report.page.list(:list).add_row do |row|
       @num = Date.parse(@to_ymd) - Date.parse(@from_ymd)
-
+      row.item(:shop_name).value(nanaco[0].shop_ryaku) 
         for i in 0..@num.to_i
-          result_date = (Date.parse(@from_ymd) + i).strftime("%Y%m%d")
-          nanaco_point_nums = get_nanaco_point_num(result_date,shop.id)
-          nanaco_points = get_nanaco_point(result_date,shop.id)
-          row.item(:shop_name).value(shop.shop_ryaku) 
-          unless nanaco_points == nil
-            row.item("p1_#{i+1}").value(nanaco_point_nums.nanacopoint_num.to_i)
-            p1_sum_col = p1_sum_col + nanaco_point_nums.nanacopoint_num.to_i
-            row.item("p2_#{i+1}").value(nanaco_points.nanacopoint.to_i)
-            p2_sum_col = p2_sum_col + nanaco_points.nanacopoint.to_i
+          #row.item(:shop_name).value(nanaco[0].shop_ryaku) 
+          unless nanaco[i] == nil
+            row.item("p1_#{i+1}").value(nanaco[i].nanacopoint_num.to_i)
+            p1_sum_col = p1_sum_col + nanaco[i].nanacopoint_num.to_i
+            row.item("p2_#{i+1}").value(nanaco[i].nanacopoint.to_i)
+            p2_sum_col = p2_sum_col + nanaco[i].nanacopoint.to_i
           end
         end #for
         row.item(:p1_sum).value(p1_sum_col)
         row.item(:p2_sum).value(p2_sum_col)
       end #add_row
-    end # shops.each
-
+    end # nanaco_datas.each_with_index
+    
     #ファイル名セット     
     #pdf_title = "#{title2}_#{@from_ymd[0,6]}.pdf"
     pdf_title = "#{title2}_#{@from_ymd}_#{@to_ymd}.pdf"
--- app/helpers/d_nanaco_point_lists_helper.rb
+++ app/helpers/d_nanaco_point_lists_helper.rb
@@ -1,36 +1,38 @@
 module DNanacoPointListsHelper
   
-  def get_nanaco_point_num(result_date,shop_id)
-    sql = <<-SQL
-      SELECT COALESCE(sum(pos1_data),0) 
-           + COALESCE(sum(pos2_data),0) 
-           + COALESCE(sum(pos3_data),0) as nanacopoint_num
-      FROM d_results a ,d_result_oiletcs b
-       where a.id = b.d_result_id
-         and b.m_oiletc_id = 31
-         and a.m_shop_id = ?
-         and a.result_date = ?
-    SQL
-    
-    nanaco_point_num = DResultOiletc.find_by_sql([sql,shop_id,result_date]).first
-    
-    return nanaco_point_num
-  end
-  
-  def get_nanaco_point(result_date,shop_id)
+  def get_nanaco_point(result_date,shop_kbn)
 
     sql = <<-SQL
-      SELECT COALESCE(sum(pos1_data),0) 
-           + COALESCE(sum(pos2_data),0) 
-           + COALESCE(sum(pos3_data),0) as nanacopoint
-      FROM d_results a ,d_result_oiletcs b
-       where a.id = b.d_result_id
-         and b.m_oiletc_id = 32
-         and a.m_shop_id = ?
-         and a.result_date = ?
+      SELECT ms.shop_cd,ms.shop_ryaku ,COALESCE(np1.nanacopoint_num,0) nanacopoint_num
+                                      ,COALESCE(np2.nanacopoint,0) nanacopoint
+      FROM m_shops ms
+      LEFT JOIN (
+                  select a.m_shop_id,COALESCE(sum(pos1_data),0) 
+                                   + COALESCE(sum(pos2_data),0) 
+                                   + COALESCE(sum(pos3_data),0) as nanacopoint_num
+                  from d_results a ,d_result_oiletcs b
+                  where a.id = b.d_result_id
+                    and b.m_oiletc_id = 31
+                    and a.result_date = ?
+                  group by a.m_shop_id
+                 ) np1 
+      ON ms.id = np1.m_shop_id
+      LEFT JOIN (
+                  select a.m_shop_id,COALESCE(sum(pos1_data),0) 
+                                   + COALESCE(sum(pos2_data),0) 
+                                   + COALESCE(sum(pos3_data),0) as nanacopoint
+                  from d_results a ,d_result_oiletcs b
+                  where a.id = b.d_result_id
+                    and b.m_oiletc_id = 32
+                    and a.result_date = ?
+                  group by a.m_shop_id
+                 ) np2 
+      ON ms.id = np2.m_shop_id
+      WHERE ms.shop_kbn = ?
+      ORDER BY ms.shop_cd
     SQL
     
-    nanaco_point = DResultOiletc.find_by_sql([sql,shop_id,result_date]).first
+    nanaco_point = DResultOiletc.find_by_sql([sql,result_date,result_date,shop_kbn])
     
     return nanaco_point
   end
