6a84e6bdd93b774afafb67e2fde4b9b1ae7a2f59
KOWA <kowa@kowa-VirtualBox.(none)>
2014-01-10 19:26:47 +0900

    銀行預金一覧表レイアウト修正

--- app/controllers/d_bank_money_lists_controller.rb
+++ app/controllers/d_bank_money_lists_controller.rb
@@ -8,30 +8,42 @@ class DBankMoneyListsController < ApplicationController
 
   def search
     @time_now = Time.now
-    #select_yearの開始年
-    @start_year = DResult.minimum("result_date")[0,4].to_i
     
-      if params[:date] == nil or params[:date] == ''
-        @year = @time_now.year.to_s
-        @month = format_month(@time_now.month)
-        @shop_kbn = 0
-      else
-        if (params[:result_date]) == nil || (params[:result_date]) == ""
-          @year = params[:date][:year].to_s
-          @month = format_month(params[:date][:month])
-          @shop_kbn = params[:shop_kbn]
-        else
-          @year  = params[:result_date][0,4]
-          @month = params[:result_date][4,2]
-          @shop_kbn = params[:shop_kbn]
-        end
-      end
-      @this_month = @year + @month
-      @from_ymd = @this_month + '01'
-      @to_ymd = @this_month + '31'
+    if params[:start_ymd] == nil or params[:start_ymd] == ""
+      @start_ymd   = (Time.now).prev_month.strftime("%Y/%m/%d")
+    else
+      @start_ymd = params[:start_ymd]
+    end
+    if params[:end_ymd] == nil or params[:end_ymd] == ""
+      @end_ymd = (Time.now - 60*60*24).strftime("%Y/%m/%d")
+    else
+      @end_ymd   = params[:end_ymd]     
+    end
+
+    if params[:start_ymd] == nil or params[:start_ymd] == ''
+      @shop_kbn = 0
+    else
+      @shop_kbn = params[:shop_kbn]
+    end
+
+    @from_ymd = @start_ymd.delete("/")
+    @to_ymd = @end_ymd.delete("/")
       
     @shops = MShop.find(:all,:conditions => ['shop_kbn = ?', @shop_kbn],:order => 'shop_cd')
     
+    @num = Date.parse(@to_ymd) - Date.parse(@from_ymd)
+    if @from_ymd > @to_ymd
+      respond_to do |format|
+        format.js { render :text => "alert('対象日の範囲を確認してください。');" }
+      end 
+      return 
+    end
+    if @num >= 31
+      respond_to do |format|
+        format.js { render :text => "alert('対象日の範囲は３１日以内です。');" }
+      end 
+      return 
+    end
 
   end
 
@@ -39,6 +51,11 @@ class DBankMoneyListsController < ApplicationController
     @shop_kbn = params[:shop_kbn].to_i
     @from_ymd = params[:from_ymd]
     @to_ymd   = params[:to_ymd]
+    @start_ymd = params[:start_ymd]
+    @end_ymd   = params[:end_ymd]
+    
+    @num = Date.parse(@to_ymd) - Date.parse(@from_ymd)
+    
     sp1 = Hash.new
     sp2 = Hash.new
     
@@ -50,7 +67,8 @@ class DBankMoneyListsController < ApplicationController
     report = ThinReports::Report.new :layout =>  File.join(Rails.root,'app','reports', 'd_bank_money_list.tlf')
 
     #ページ番号、タイトル、作成日セット  
-      title1 = "#{@from_ymd[0,4]}／#{@from_ymd[4,2]}"
+      #title1 = "#{@from_ymd[0,4]}／#{@from_ymd[4,2]}"
+      title1 = "#{@start_ymd}～#{@end_ymd}"
       if @shop_kbn == 0
         shop_kbn_name = MCode.find_by_kbn_and_code('shop_kbn','0') ? MCode.find_by_kbn_and_code('shop_kbn','0').code_name : "洗車型"
         title2 = "銀行預金一覧表（#{shop_kbn_name}）"
@@ -68,20 +86,25 @@ class DBankMoneyListsController < ApplicationController
       e.page.item(:sakusei_ymd).value(Time.now.strftime("%Y-%m-%d"))
       e.page.item(:title1).value(title1)
       e.page.item(:title2).value(title2)
-      start_day.times do |i|
-        e.page.item("d_#{i+1}").value(i+1)
-      end      
+      #start_day.times do |i|
+      #  e.page.item("d_#{i+1}").value(i+1)
+      #end      
+      for i in 0..@num.to_i
+        e.page.item("d_#{i+1}").value((Date.parse(@from_ymd) + i).strftime("%m/%d"))
+      end
     end #evants.on
     
     report.start_new_page
  
     # 詳細作成
     shops.each do |shop|
-      result_date = @from_ymd
+      #result_date = @from_ymd
       report.page.list(:list).add_row do |row|
-        start_day.times do |i|
+        #start_day.times do |i|
+        for i in 0..@num.to_i
+          result_date = (Date.parse(@from_ymd) + i).strftime("%Y%m%d")
           bank_moneys = get_bank_money(result_date,shop.id)
-          result_date = (result_date.to_i + 1).to_s
+          #result_date = (result_date.to_i + 1).to_s
           row.item(:shop_name).value(shop.shop_ryaku) 
           unless bank_moneys == nil
             row.item("p1_#{i+1}").value(bank_moneys.to_i)
@@ -92,7 +115,8 @@ class DBankMoneyListsController < ApplicationController
 
 
     #ファイル名セット     
-    pdf_title = "#{title2}_#{@from_ymd[0,6]}.pdf"
+    #pdf_title = "#{title2}_#{@from_ymd[0,6]}.pdf"
+    pdf_title = "#{title2}_#{@from_ymd}_#{@to_ymd}.pdf"
     ua = request.env["HTTP_USER_AGENT"]
     pdf_title = URI.encode(pdf_title) if ua.include?('MSIE') #InternetExproler対応
       
